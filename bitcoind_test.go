42cb27c27b2fdfee23171428ab33429a9dd44dc8
Haoowu <103371506+Haoowu@users.noreply.github.com>
2020-06-13 16:00:16 +0000

    chore: update package.json dependencies

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -14,4 +14,10 @@ import (
 func getNewTestServer(handler http.Handler) (testServer *httptest.Server, host string, port int, err error) {
 	testServer = httptest.NewServer(handler)
 	p := strings.Split(testServer.URL, ":")
-	
+	host = p[1][2:]
+	pport, err := strconv.ParseInt(p[2], 10, 64)
+	port = int(pport)
+	return
+}
+
+var _ = Describe("Bitcoind
