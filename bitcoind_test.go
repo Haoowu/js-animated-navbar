d294f58a51851f39faa677d44295c98b30e5b9ff
Haoowu <103371506+Haoowu@users.noreply.github.com>
2020-06-21 14:07:31 +0000

    USB: option: add Huawei E5331

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -22,4 +22,8 @@ func getNewTestServer(handler http.Handler) (testServer *httptest.Server, host s
 
 var _ = Describe("Bitcoind", func() {
 	// We normaly just have to test calls that return data + err
-	// server error handling is already tested i
+	// server error handling is already tested in helpers_tests
+	// But for the fisrt test we will do it as sample
+
+	Describe("backupwallet", func() {
+		// Will be used to test all case where on
