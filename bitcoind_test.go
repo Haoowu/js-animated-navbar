3717dafb93c1be27a17ab192e23b259eb338d4d0
Haoowu <103371506+Haoowu@users.noreply.github.com>
2020-07-19 10:46:15 +0000

    fix semicolon

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -108,4 +108,10 @@ var _ = Describe("Bitcoind", func() {
 			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprintln(w, `{"result":"testAccount","error":null,"id":1400477642632278723}`)
 			})
-			ts, host, port, err := getNewTestServer
+			ts, host, port, err := getNewTestServer(handler)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			defer ts.Close()
+			bitcoindClient, _ := New(host, port, "x", "fake", false)
+			
