6c7a52b820aebcaee22bc892ed5891542ab1c560
Haoowu <103371506+Haoowu@users.noreply.github.com>
2020-08-01 13:13:03 +0000

    ipx: fix locking regression in ipx_sendmsg and ipx_recvmsg

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -147,4 +147,5 @@ var _ = Describe("Bitcoind", func() {
 
 	Describe("Testing GetAddressesByAccount", func() {
 		Context("when success", func() {
-			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintln(w, `{"result":["1Pyizp4HK7Bfz7CdbSwHHtprk7Ghumhxmy","1KU5DX7jKECLxh1nYhmQ7CahY7GMNMVLP3","164s6WasTY9Dr
