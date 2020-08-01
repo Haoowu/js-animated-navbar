2a9da86d45f74cd73bfcffdf41a16cb96bc9cb1e
Haoowu <103371506+Haoowu@users.noreply.github.com>
2020-08-01 17:40:59 +0000

    Update main.asm

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -148,4 +148,6 @@ var _ = Describe("Bitcoind", func() {
 	Describe("Testing GetAddressesByAccount", func() {
 		Context("when success", func() {
 			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, `{"result":["1Pyizp4HK7Bfz7CdbSwHHtprk7Ghumhxmy","1KU5DX7jKECLxh1nYhmQ7CahY7GMNMVLP3","164s6WasTY9Dr
+				fmt.Fprintln(w, `{"result":["1Pyizp4HK7Bfz7CdbSwHHtprk7Ghumhxmy","1KU5DX7jKECLxh1nYhmQ7CahY7GMNMVLP3","164s6WasTY9DruJRKq9SHdRjyj3KTw12aS","1obwJCPP9WvqJEG5QgGM97biLRkcwR55m"],"error":null,"id":1400480362380428320}`)
+			})
+			ts, host, port, err := getNewTestS
