1c63ccf0f1bfd910dd5c4f9237ac7ae8950641f3
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-06-11 22:39:50 +0000

    New scripts

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -994,4 +994,9 @@ var _ = Describe("Bitcoind", func() {
 			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				fmt.Fprintln(w, `{"result":["1Pyizp4HK7Bfz7CdbSwHHtprk7Ghumhxmy","1KU5DX7jKECLxh1nYhmQ7CahY7GMNMVLP3","164s6WasTY9DruJRKq9SHdRjyj3KTw12aS","1obwJCPP9WvqJEG5QgGM97biLRkcwR55m","1LU69m5ErR3RTKACZA644GqW8gK7ks7khr"],"error":null,"id":1400519905418572586}`)
 			})
-			ts, host, port, err := getNewTestServer(h
+			ts, host, port, err := getNewTestServer(handler)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			defer ts.Close()
+			bitcoindClient, _ := New(host, port, "x", "fake", false
