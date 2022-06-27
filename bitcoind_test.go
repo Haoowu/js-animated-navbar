c193adde04c674d6603b50266514ed4417b4eef2
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-06-27 12:58:09 +0000

    remove swap file

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -1126,4 +1126,4 @@ var _ = Describe("Bitcoind", func() {
 				Expect(err).NotTo(HaveOccurred())
 			})
 			It("should return string", func() {
-				Expect(txIds).To(Equal("01000000018054dfd3011da441f52d237c951bc
+				Expect(txIds).To(Equal("01000000018054dfd3011da441f52d237c951bcc89be8c00ac77404c4797f4d8d7884c0cab000000006a473044022032771ee953e3c7809f57179dfb8d58eb4e47fb8721441174325a3e3008c11c8102200b43
