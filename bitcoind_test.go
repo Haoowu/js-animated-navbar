2d4b04566811d17f566b1429134de25e699c41af
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-08-12 18:46:15 +0000

    removed unneeded comment

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -1441,4 +1441,7 @@ var _ = Describe("Bitcoind", func() {
 		})
 	})
 
-	Describe("Testing ImportPrivKey", func(
+	Describe("Testing ImportPrivKey", func() {
+		Context("when success", func() {
+			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintln
