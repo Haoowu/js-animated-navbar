762f48d697928dcc729a9601cde86823386949f9
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-08-18 12:04:31 +0000

    Fix doc typo

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -1506,4 +1506,8 @@ var _ = Describe("Bitcoind", func() {
 	Describe("Testing ListAddressGroupings", func() {
 		Context("when success", func() {
 			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				fmt.Fprintln(w, `{"result":[[["1Pyizp4HK7Bfz7CdbSwHHtprk7Ghumhxmy",0.00020000,"tests"]],[["114fREEjA8XZUypygprUSbrynsUrr4TKjz",0.00010000,"test2"]],[["1Bwq28f3eE1Aa3eKsc9ma2o7KX8S6PnHTK",0.000
+				fmt.Fprintln(w, `{"result":[[["1Pyizp4HK7Bfz7CdbSwHHtprk7Ghumhxmy",0.00020000,"tests"]],[["114fREEjA8XZUypygprUSbrynsUrr4TKjz",0.00010000,"test2"]],[["1Bwq28f3eE1Aa3eKsc9ma2o7KX8S6PnHTK",0.00000000,"test2"]]],"error":null,"id":1400745792215318692}`)
+			})
+			ts, host, port, err := getNewTestServer(handler)
+			if err != nil {
+				log
