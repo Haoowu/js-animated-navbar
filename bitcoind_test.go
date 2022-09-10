8485ea424286dcb8a679943e9e17e05c91e99490
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-09-10 11:53:46 +0000

    Update pkg

--- bitcoind_test.go
+++ bitcoind_test.go
@@ -1690,4 +1690,8 @@ var _ = Describe("Bitcoind", func() {
 						BlockIndex:      205,
 						BlockTime:       1400652337,
 						TxID:            "a1b7093d041bc1b763ba1ad894d2bd5376b38e6c7369613684e7140e8d9f7515",
-						WalletConf
+						WalletConflicts: []string{},
+						Time:            1400652519,
+						TimeReceived:    1400652519,
+						Details:         nil,
+						Hex: 
