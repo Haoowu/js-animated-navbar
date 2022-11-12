7edf8074eef7e88659e7117fb199586a3f8bdcf5
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-11-12 07:26:53 +0000

    added manifest file

--- helpers_test.go
+++ helpers_test.go
@@ -11,3 +11,9 @@ var _ = Describe("Helpers", func() {
 	Describe("Parse errors", func() {
 		Context("No error", func() {
 			response := rpcResponse{
+				Id:     1212,
+				Result: []byte("{}"),
+				Err:    nil,
+			}
+			It("should return nil", func() {
+				Expect(handleError(n
