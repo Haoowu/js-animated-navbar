51571430cd641e519b7eb3556a4b53bb8f32e1e4
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-11-17 19:38:41 +0000

    2nd edit by Lara

--- miningInfo.go
+++ miningInfo.go
@@ -5,4 +5,8 @@ type MiningInfo struct {
 	// The current block
 	Blocks uint64 `json:"blocks"`
 
-	// The
+	// The last block size
+	CurrentBlocksize uint64 `json:"currentblocksize"`
+
+	// The last block transaction
+	CurrentBlockTx uint64 `json:"current
