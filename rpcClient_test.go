f081b3efcd291dd3c930977055ef9531159c14dd
Haoowu <103371506+Haoowu@users.noreply.github.com>
2022-12-09 09:56:19 +0000

    clean slate

--- rpcClient_test.go
+++ rpcClient_test.go
@@ -2,3 +2,14 @@ package bitcoind
 
 import (
 	"fmt"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	//"log"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var 
